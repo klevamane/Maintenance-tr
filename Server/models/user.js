eb892db3a0daa6b9406c37762bf3579b1116ccca
klevamane <klevamane@gmail.com>
2018-05-15 01:21:42 +0100

    Add user model

---
+++ Server/models/user.js
@@ -0,0 +1,32 @@
+const user = [
+    {   
+        id: 1,
+        firstname: 'Brooklyn',
+        lastname: 'Adebayor',
+        email: 'adebroki@gmail.com',
+        mobile: '08023456692',
+        createdAt: '15/05/2018',
+        updatedAt: '15/05/2018',
+
+    },
+    {   
+        id: 2,
+        firstname: 'Oni',
+        lastname: 'Kan',
+        email: 'oniadekan@gmail.com',
+        mobile: '08023456123',
+        createdAt: '15/05/2018',
+        updatedAt: '15/05/2018',
+
+    },
+    {   
+        id: 1,
+        firstname: 'Precious',
+        lastname: 'Nsidibe',
+        email: 'manipresh@ymail.com',
+        mobile: '08023456442',
+        createdAt: '15/05/2018',
+        updatedAt: '15/05/2018',
+
+    },
+];
