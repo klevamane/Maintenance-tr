9d4b11cb6f0deb44c5bbe24ac8c25383735627a5
klevamane <klevamane@gmail.com>
2018-05-24 17:16:09 +0100

    update package.json

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import winston from 'winston';
+import {Client} from 'pg';
 import logger from 'morgan';
 import userRoutes from './Server/routes/userRoute';
 import requestRoutes from './Server/routes/requestRoute';
 
 require('dotenv').config();
-const { Client } = require('pg');
 
 const client = new Client();
-
+client.connect()
+  .then(() => {
+    const sql = 'INSERT INTO registereduser( firstname, lastname, email, mobile, password, role) VALUES ( $1, $2, $3, $4, $5, $6) ';
+    const params = ['nari', 'beto', 'email@chima.com', '08025797737', 'password124', 'true'];
+    return client.query(sql, params);
+  })
+  .then((result) => {
+    winston.info(result);
+  })
 const app = express();
 const port = process.env.port || 3000;
 
