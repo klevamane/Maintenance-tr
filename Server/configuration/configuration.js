1c0e8ed164431613d030e091efdaca786c42e85a
klevamane <klevamane@gmail.com>
2018-05-29 19:22:42 +0100

    Fix database name

--- Server/configuration/configuration.js
+++ Server/configuration/configuration.js
@@ -7,8 +7,8 @@ const configuration = {
   development: {
     user: process.env.LOCALDBUSER,
     password: process.env.LOCALDBPSWD,
-    // database: 'dbtester',
-    database: 'testonly',
+    database: 'dbtester',
+    //database: 'testonly',
     host: process.env.LOCALDBHOST,
     port: process.env.LOCALDBSERVERPORT,
     max: 10,
