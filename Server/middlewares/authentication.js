506bef490b24f0c0f93487cf290c403a785fdf9b
klevamane <klevamane@gmail.com>
2018-05-30 05:41:37 +0100

    Remove log statements

--- Server/middlewares/authentication.js
+++ Server/middlewares/authentication.js
@@ -9,11 +9,9 @@ exports.checkAuthentication = (req, res, next) => {
     // jwt.verify throuws and error if it fails
     // which will be sent to the catch block
     const decoded = jwt.verify(token, 'secreteKey');
-    const decodededtoken = jwt.decode(token);
+    // const decodededtoken = jwt.decode(token);
     // Add a new property to the request body
     req.decodedUserData = decoded;
-    winston.info(`Checking decoded token ${decodededtoken.id}`);
-    winston.info(`checking andoth ${req.decodedUserData.id}`);
     next();
   } catch (error) {
     return res.status(401).json({
