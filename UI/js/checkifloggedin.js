f1307c9dc1e3ffb1ffd39d63e27e5cd1c3da41c4
Onengiye <klevamane@gmail.com>
2018-06-13 08:17:42 +0100

    Add feature to create a new request

--- UI/js/checkifloggedin.js
+++ UI/js/checkifloggedin.js
@@ -18,13 +18,13 @@ if(errorDetection !== true) {
 
   const headers = new Headers({'Content-Type': 'application/json'});
   headers.append('Authorization', `Bearer ${token}`);
-    fetch('https://maintenancetr.herokuapp.com/api/v1/users/requests', {
-  method: 'GET',
-  headers
+  fetch('https://maintenancetr.herokuapp.com/api/v1/users/requests', {
+    method: 'GET',
+    headers
 }).then((response) => {
   if(response.status === 401) {
     window.location.replace('./Index.html');
   }
   })
-  .catch(error => console.log(err));
+  .catch(err => console.log(err));
 }
---
+++ UI/js/createrequest.js
@@ -0,0 +1,44 @@
+var newRequestSubmitButton = document.getElementById('usersubmit');
+newRequestSubmitButton.addEventListener('click',function(event) {
+const requestForm = document.getElementById('requestform');
+    event.preventDefault();
+    let checker;
+    // getuser data from client
+    retrievedUserDataFromLocalStorage = localStorage.getItem('dataAccessibleToOtherPages');
+    token = JSON.parse(retrievedUserDataFromLocalStorage).token;
+    let faultValue = document.getElementById('fault');
+    let fault = faultValue.options[faultValue.selectedIndex].value;
+    let brand = document.getElementById('brand').value;
+    let modelnumber = document.getElementById('model').value;
+    let description = document.getElementById('description').value;
+    let otherValue = document.getElementById('type')
+    let other = otherValue.options[otherValue.selectedIndex].value;
+
+    const headers = new Headers({'Content-Type': 'application/json'});
+    headers.append('Authorization', `Bearer ${token}`);
+    fetch('https://maintenancetr.herokuapp.com/api/v1/users/requests', {
+        method: 'POST',
+        body: JSON.stringify({
+          brand,
+          modelnumber,
+          fault,
+          other,
+          description  
+        }),
+        headers
+        }).then((response) => {
+           if(response.status === 201) {
+               toastr.success('Request has been created');
+               checker = true;
+               requestForm.reset();
+           } 
+           return response.json();
+        }).then((result) => {
+            // Error exists if checker !== true
+            if(checker !== true) {
+            // extract the object value from the key, to determine the validation error throw by the api validator
+            const displayError = Object.values(data);
+            toastr.error(displayError);
+            }
+        })
+});
